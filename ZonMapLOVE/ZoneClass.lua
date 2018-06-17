b45621beef0df53616fdd9a2e879b836ffa58d00
Nedok <FredrikHam@Hotmail.com>
2018-06-17 21:15:52 +0200

    s

--- ZonMapLOVE/ZoneClass.lua
+++ ZonMapLOVE/ZoneClass.lua
@@ -2,82 +2,86 @@
 
 local Zon = {}
 
-    function Zon:new()
 
-        -- Sektormiljö
+function Zon.getNewSectorEnvierment()
+    local Sec = {
+        { C = 0.22, RL = 0, Data = {Des = "Skog eller helt övervuxna ruiner.", Hot = true,  Artifakt = false}},
+        { C = 0.18, RL = 0, Data = {Des = "Totalt raserade ruiner, grusöken.", Hot = true,  Artifakt = false}},
+        { C = 0.18, RL = 0, Data = {Des = "Delvis raserade ruiner.",           Hot = true,  Artifakt = true }},
+        { C = 0.22, RL = 0, Data = {Des = "Välbevarade ruiner.",               Hot = true,  Artifakt = true }},
+        { C = 0.18, RL = 1, Data = {Des = "Öde industrilandskap.",             Hot = true,  Artifakt = true }},
+        { C = 0.02, RL = 0, Data = {Des = "Bosättning",                        Hot = false, Artifakt = false}}
+    }
 
-        local Sec = {
-            { C = 0.22, RL = 0, Data = {Des = "Skog eller helt övervuxna ruiner.", Hot = true,  Artifakt = false}},
-            { C = 0.18, RL = 0, Data = {Des = "Totalt raserade ruiner, grusöken.", Hot = true,  Artifakt = false}},
-            { C = 0.18, RL = 0, Data = {Des = "Delvis raserade ruiner.",           Hot = true,  Artifakt = true }},
-            { C = 0.22, RL = 0, Data = {Des = "Välbevarade ruiner.",               Hot = true,  Artifakt = true }},
-            { C = 0.18, RL = 1, Data = {Des = "Öde industrilandskap.",             Hot = true,  Artifakt = true }},
-            { C = 0.02, RL = 0, Data = {Des = "Bosättning",                        Hot = false, Artifakt = false}}
-        }
+    local SR = math.random()
 
-
-        local SR = math.random()
-
-        local i = 1
-        while(SR > 0) do
-            SR = SR-Sec[i].C
-            i = i + 1
-        end
-
-        self["Sektormiljö"] = Sec[i].Data
-
-        -- Dominerande ruin
-        local Ruiner = {}
-        Ruiner[0] = {
-            "Affärsgalleria",    "Badhus",
-            "Bensinstation",     "Biograf",
-            "Bostadsområde",     "Busstation",
-            "Flygplansvrak",     "Förvildad park",
-            "Höghusruin",        "Jaktaffär",
-            "Kontorskomplex",    "Krater",
-            "Kråkslott",         "Kyrka",
-            "Lekplats",          "Miljonprogram",
-            "Motorväg",          "Museum",
-            "Nöjesfält",         "Parkeringshus",
-            "Polisstation",      "Radiostation",
-            "Raserad bro",       "Skyddsrum",
-            "Slagfält",          "Snabbmatsresturang",
-            "Sporthall",         "Sjukhus",
-            "Stridsvagn",        "Teater",
-            "Tunnelbana",        "Tågstation",
-            "Snabbköp",          "Vägtunnel",
-            "Övergiven skola",   "Ödelagd marina"
-        }
-
-        Ruiner[1] = {
-            "Fabrik",
-            "Fartygsvrak",
-            "Kraftledning",
-            "Militärbas",
-            "Pipline",
-            "Radiomast",
-            "Raffinaderi",
-            "Reningsverk",
-            "Skutbana",
-            "Soptipp",
-            "Vindkraftverk",
-            "Oljecistern"
-        }
-
-
-        local Temp = math.random(1, #Ruiner[Sec[i].RL])
-        self["DominantRuin"] = Ruiner[Sec[i].RL][Temp]
-
-        -- Bestäm rötnivå
-
-        self["Rötnivå"] = (  { 0, 1, 1, 1, 1, 2 }  )[math.random(1, 6)]
-
-
-        -- Bestäm Hotnivå
-
-
-
+    local i = 1
+    while(SR > 0) do
+        SR = SR-Sec[i].C
+        i = i + 1
     end
+
+    return {table.unpack(Sec[i].Data)}, Sec[i].RL
+end
+
+function getRandomElement(TableOfElements)
+    local Key = {}
+    for k in pairs(TableOfElements) do
+        table.insert(Key, k)
+    end
+
+    Key = Keys[math.random(#Key)]
+
+    return TableOfElements[Key]
+end
+
+function Zon.getNewRuinEnvierment(RuinType)
+    local Ruiner = {}
+    Ruiner[0] = {
+        "Affärsgalleria",    "Badhus",             "Bensinstation",     "Biograf",
+        "Bostadsområde",     "Busstation",         "Flygplansvrak",     "Förvildad park",
+        "Höghusruin",        "Jaktaffär",          "Kontorskomplex",    "Krater",
+        "Kråkslott",         "Kyrka",              "Lekplats",          "Miljonprogram",
+        "Motorväg",          "Museum",             "Nöjesfält",         "Parkeringshus",
+        "Polisstation",      "Radiostation",       "Raserad bro",       "Skyddsrum",
+        "Slagfält",          "Snabbmatsresturang", "Sporthall",         "Sjukhus",
+        "Stridsvagn",        "Teater",             "Tunnelbana",        "Tågstation",
+        "Snabbköp",          "Vägtunnel",          "Övergiven skola",   "Ödelagd marina"
+    }
+
+    Ruiner[1] = {
+        "Fabrik",   "Fartygsvrak", "Kraftledning",  "Militärbas",
+        "Pipline",  "Radiomast",   "Raffinaderi",   "Reningsverk",
+        "Skutbana", "Soptipp",     "Vindkraftverk", "Oljecistern"
+    }
+
+    return getRandomElement(Ruiner[RuinType])
+
+end
+
+
+function Zon:new()
+
+    -- Sektormiljö
+
+    local RL = 0
+    self["Sektormiljö"], RL = Zon.getNewSectorEnvierment()
+
+
+    -- Dominerande ruin
+
+    self["DominantRuin"] = getNewRuinEnvierment(RL)
+
+    -- Bestäm rötnivå
+
+    self["Rötnivå"] = (  { 0, 1, 1, 1, 1, 2 }  )[math.random(1, 6)]
+
+
+    -- Bestäm Hotnivå
+end
+
+
+
 
 
 
